639dc7becaf46cad8a6b31afd323c9afab636524
Mario Florez <mario.florez249@gmail.com>
2025-04-22 00:03:16 -0500

    Se instala el paquete body-parser con npm. Se le da soporte a través de app.js bodyParser y se asigna el uso a la app para gestionar peticiones json bajo el protocolo http.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3000;
 
