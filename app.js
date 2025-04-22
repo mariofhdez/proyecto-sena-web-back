b763b28ec4adfc535938ed998d614b1150a0a289
Mario Florez <mario.florez249@gmail.com>
2025-04-22 00:05:00 -0500

    se da soporte a el envío de formularios

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
 
