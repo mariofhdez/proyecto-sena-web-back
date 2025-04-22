38ac526b26e190d2200d395c6cce03a2cc759bc7
Mario Florez <mario.florez249@gmail.com>
2025-04-21 23:55:22 -0500

    Se realizaron los ajustes iniciales en el proyecto. En app.js se creó con express la aplicación agregando la configuración del puerto y un mensaje por consola que indique que el proyecto está activo. Se crearon los ficheros .env y .env-example para gestionar las variables de entorno del proyecto. Se agregó al fichero app.js la librería dotenv para que el aplicativo gestione y de seguimiento a las variables de entorno. Se creó el archivo .gitignore para que al cargar el repositorio a github no envíe información innecesaria. Se agregaron scripts al archivo package.json para comandos repetitivos como correr el servidor en pruebas o producción.

---
+++ app.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res)=>{
+    res.send(`
+        <h1>Aplicación Versión 2</h1>
+        `)
+});
+
+
+app.listen(PORT, () =>{
+console.log(`Servidor: http://localhost:${PORT}`)
+});
