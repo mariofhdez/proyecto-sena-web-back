25072da1763cf90610543eeb6efb92740431ed98
Mario Florez <mario.florez249@gmail.com>
2025-04-22 22:24:39 -0500

    Nueva funcionalidad. Se agregan dos servicios para la gestión de usuarios a través del GET para consultas y el POST para crear usuarios. Se usan validaciones en el campo id para que no existan registros repetidos, en el campo name para que los nombres tengan una longitud mínima y en el campo email para que cumpla con la estructura básica de un correo.

--- app.js
+++ app.js
@@ -2,24 +2,28 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const fs = require('fs');
+const path = require('path');
+const usersFilePath = path.join(__dirname, 'users.json');
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send(`
         <h1>Aplicación Versión 2</h1>
         `)
 });
 
-app.get('/users/:id', (req, res)=>{
+app.get('/users/:id', (req, res) => {
     const userId = req.params.id;
     res.send(`Mostrar información del usuario con ID: ${userId}`)
 });
 
-app.get('/search', (req, res) =>{
+app.get('/search', (req, res) => {
     const terms = req.query.termino || 'No especificado';
     const category = req.query.category || 'Todas';
 
@@ -42,11 +46,11 @@ app.post('/form', (req, res) => {
     });
 });
 
-app.post('/api/data', (req, res) =>{
+app.post('/api/data', (req, res) => {
     const data = req.body;
 
-    if(!data || Object.keys(data).length === 0){
-        return res.status(400).json({error: 'No se recibieron datos'});
+    if (!data || Object.keys(data).length === 0) {
+        return res.status(400).json({ error: 'No se recibieron datos' });
     }
 
     res.status(200).json({
@@ -54,9 +58,69 @@ app.post('/api/data', (req, res) =>{
         data
     });
 
+});
+
+app.get('/users', (req, res) => {
+    fs.readFile(usersFilePath, 'utf-8', (err, data) => {
+        if (err) {
+            return res.status(500).json({ error: 'Error con la conexión de datos' })
+        }
+        const users = JSON.parse(data);
+        res.json(users);
+    })
 })
 
+app.post('/users', (req, res) => {
+    const newUser = req.body;
+    const id = newUser.id;
+    const name = newUser.name;
+    const email = newUser.email;
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-app.listen(PORT, () =>{
-console.log(`Servidor: http://localhost:${PORT}`)
+    // let users = [];
+
+    if (!newUser || Object.keys(newUser).length === 0) {
+        return res.status(400).json({ error: 'No se recibieron datos' });
+    }
+
+    if (name.length < 3) {
+        return res.status(400).json({ error: 'El campo \'name\' debe ser igual o mayor a 3 caracteres' })
+    }
+
+    if (!regex.test(email)) {
+        return res.status(400).json({ error: 'El campo \'email\' no cumple con la estructura esperada de un correo' })
+    }
+
+    fs.readFile(usersFilePath, 'utf-8', (err, data) => {
+        if (err) {
+            return res.status(400).json({ error: 'Error con la conexión de datos' })
+        }
+
+        const users = JSON.parse(data);
+
+        const isRegistered = users.find(user => user.id == id);
+
+        if(!isRegistered){
+            users.push(newUser);
+        }else{
+            return res.status(400).json({ error: 'El \'id\' ya se encuentra registrado en nuestra base de datos' })
+        }
+
+
+        fs.writeFile(usersFilePath, JSON.stringify(users, null, 2), (err) => {
+            if (err) {
+                return res.status(500).json({ error: 'Error al guardar usuario' })
+            }
+            res.status(201).json(newUser);
+        })
+    });
+
+    // console.log(users.length);
+
+
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor: http://localhost:${PORT}`)
 });
