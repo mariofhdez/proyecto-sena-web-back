11a250bec1d2e86bf64c97dddfafb30c1e7c859b
Mario Florez <mario.florez249@gmail.com>
2025-04-28 21:50:49 -0500

    FIXBUG. Se corrige error presentado al momento de lanzar errores en el registro y login de usuario, brindando en la respuesta una estructura homogénea y clara

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,7 +6,11 @@ exports.register = async(req, res) => {
         await registerService(email, name, password, role);
         res.status(201).json({ message: `User: ${name} was created succesfully!`});
     } catch (error) {
-        return res.status(500).json({error: error.message});
+        if(error.code === 'AU001'){
+            return res.status(400).json({ code: error.code, error: error.message})
+        }
+
+        return res.status(500).json({code: "500", error: error.message});
     }
 }
 
@@ -16,6 +20,10 @@ exports.login = async (req, res) => {
         const token = await loginService(email, password);
         return res.status(200).json({ message: token });
     } catch (error) {
-        return res.status(500).json({error: error.message});
+        if(error.code === 'AU002'){
+            return res.status(400).json({ code: error.code, error: error.message})
+        }
+
+        return res.status(500).json({code: "500", error: error.message});
     }
 }
--- src/services/authService.js
+++ src/services/authService.js
@@ -5,38 +5,45 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 exports.registerService = async (email, name, password, role) => {
-    try {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = await prisma.user.create({
-            data: {
-                email,
-                name,
-                password: hashedPassword,
-                role,
-                isActive: 'TRUE'
-            }
-        });
-        return newUser;
-    } catch (error) {
-        if(error.code === "P2002"){
-            throw new Error('Este correo ya está resgistrado');
-        }
-        throw new Error('Error al crear usuario.');
+    const existingUser = await prisma.user.findFirst({ where: {email}});
+    if(existingUser){
+        const error = new Error('Este correo ya está registrado');
+        error.name = 'Correo duplicado';
+        error.code = 'AU001';
+        throw error;
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await prisma.user.create({
+        data: {
+            email,
+            name,
+            password: hashedPassword,
+            role,
+            isActive: 'TRUE'
+        }
+    });
+    return newUser;
+
 }
 
 exports.loginService = async (email, password) => {
-    try {
-        const user = await prisma.user.findUnique({ where: { email: email }});
-        if(!user) return res.status(400).json({ error: 'Usuario o contraseña invalida'});
-
-        const validPassword = await bcrypt.compare(password, user.password);
-        if(!validPassword) return res.status(400).json({ error: 'Usuario o contraseña invalida'});
-
-        const token = jwt.sign({ id: user.id, user: user.email, role: user.role, isActive: user.isActive}, process.env.JWT_SECRET, {expiresIn: '4h'});
-        return token;
-    } catch (error) {
-        throw new Error(error);
-        // throw new Error('Error al autenticarse');
+    const user = await prisma.user.findUnique({ where: { email: email } });
+    if (!user) {
+        const error = new Error('Usuario o contraseña inválidas');
+        error.name = 'Credenciales incorrectas';
+        error.code = 'AU002';
+        throw error;
     }
+
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+        const error = new Error('Usuario o contraseña inválidas');
+        error.name = 'Credenciales incorrectas';
+        error.code = 'AU002';
+        throw error;
+    }
+
+    const token = jwt.sign({ id: user.id, user: user.email, role: user.role, isActive: user.isActive }, process.env.JWT_SECRET, { expiresIn: '4h' });
+    return token;
 }
